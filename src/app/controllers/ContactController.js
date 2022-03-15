4d68b5d3e8038298f5c6cf6e1f8bee8af0fa0256
Lucas Chitolina <lucas.chitolina01@gmail.com>
2022-03-15 19:08:40 -0300

    :tada: implements show route and delete route - class: Criando as rotas de show e delete - module: 4

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -7,8 +7,15 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
 
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    return response.json(contact);
   }
 
   store() {
@@ -19,8 +26,18 @@ class ContactController {
 
   }
 
-  delete() {
+  async delete(request, response) {
+    const { id } = request.params;
 
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactsRepository.delete(id);
+
+    return response.sendStatus(204);
   }
 }
 
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,19 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'John',
     email: 'john@example.com',
     phone: '123-456-1234',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Meredith',
+    email: 'meredith@example.com',
+    phone: '654-321-4321',
+    category_id: v4(),
   },
 ];
 
@@ -14,6 +21,20 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    // eslint-disable-next-line no-promise-executor-return
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id === id);
+      resolve();
     });
   }
 }
