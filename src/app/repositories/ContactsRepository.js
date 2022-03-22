73b6fdcae29309585dcd8eeaf295e776566f8767
Lucas Chitolina <lucas.chitolina01@gmail.com>
2022-03-22 19:32:04 -0300

    :tada: recreates find methods (all, email, id) - class: Refatorando os métodos de find - module: 4

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -20,17 +20,15 @@ let contacts = [
 ];
 
 class ContactsRepository {
-  findAll() {
-    return new Promise((resolve) => {
-      resolve(contacts);
-    });
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+    return rows;
   }
 
-  findById(id) {
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+
+    return row;
   }
 
   delete(id) {
@@ -72,11 +70,9 @@ class ContactsRepository {
     });
   }
 
-  findByEmail(email) {
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email),
-    ));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+    return row;
   }
 }
 
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE mycontacts;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS categories (
+CREATE TABLE categories (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL
 );
