a30eec50e8a9cb388c3ec1c490fa1b13aa10902a
Lucas Chitolina <lucas.chitolina01@gmail.com>
2022-03-22 21:02:02 -0300

    :tada: refactor delete method - class: Refatorando o método delete - module: 4

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database/index');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'John',
-    email: 'john@example.com',
-    phone: '123-456-1234',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Meredith',
-    email: 'meredith@example.com',
-    phone: '654-321-4321',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -32,11 +13,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id === id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 
   async create({
