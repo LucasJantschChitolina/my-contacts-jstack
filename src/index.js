e0f854c205a969e087a3687cb30537eb9f749911
Lucas Chitolina <lucas.chitolina01@gmail.com>
2022-03-28 20:10:38 -0300

    :tada: adding error handling - class: Capturando e tratando erros - module: 4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,6 +8,13 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  console.log('##### Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 const port = 3000;
 
